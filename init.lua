6aa155ca5014fd731296bbcedb89427704a5433e
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2025-12-18 23:29:22 +0100

    Added working clipboard for wsl

--- init.lua
+++ init.lua
@@ -75,6 +75,19 @@ vim.o.wrap = false
 
 vim.g.netrw_browsex_viewer = 'sensible-browser'
 
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
